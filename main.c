316e4f0d6cd81efd2551ccf629072cdf063495e0
tspnk12 <46029856+tspnk12@users.noreply.github.com>
2019-09-30 17:08:43 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,44 +1,39 @@
-/*
- * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       example application for setting a periodic wakeup
- *
- * @author      Kaspar Schleiser <kaspar@schleiser.de>
- *
- * @}
- */
-
-#include <stdio.h>
+// Подключение библиотек
 #include "xtimer.h"
 #include "timex.h"
 #include "periph/gpio.h"
 
-/* set interval to 1 second */
-#define INTERVAL (1U * US_PER_SEC)
+
+// Обработчик прерывания по нажатию кнопки
+void btn_handler(void *arg)
+{
+  // Прием аргументов, передаваемых из главного потока.
+  (void)arg;
+  // Переключение состояния пина PC8
+  gpio_toggle(GPIO_PIN(PORT_C, 8));
+  xtimer_usleep 	(100000);
+gpio_toggle(GPIO_PIN(PORT_C, 9));
+  xtimer_usleep 	(100000);
+
+}
+
 
 int main(void)
 {
-    xtimer_ticks32_t last_wakeup = xtimer_now();
-	int i = 0;
-    while(i < 1) {
-	gpio_init(GPIO_PIN(PORT_B, 0), GPIO_OUT);
-	gpio_set(GPIO_PIN(PORT_B, 0));
-	gpio_clear(GPIO_PIN(PORT_B, 0));
-	gpio_toggle(GPIO_PIN(PORT_B, 0));
-        xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
-        printf("slept until %" PRIu32 "\n", xtimer_usec_from_ticks(xtimer_now()));
-	i++;
-    }
+  // Инициализация пина PA0, к которому подключена кнопка, как источника прерывания.
+  // GPIO_RISING - прерывание срабатывает по фронту
+  // btn_handler - имя функции обработчика прерывания
+  // NULL - ничего не передаем в аргументах
+  gpio_init_int(GPIO_PIN(PORT_A, 0), GPIO_IN, GPIO_RISING, btn_handler, NULL);
+  // Инициализация пина PC8 на выход
+	gpio_init(GPIO_PIN(PORT_C, 8), GPIO_OUT);
+        gpio_init(GPIO_PIN(PORT_C, 9), GPIO_OUT);
 
-    return 0;
+  while(1)
+{
+gpio_toggle(GPIO_PIN(PORT_C, 9));
+  xtimer_usleep 	(100000);
+
+}
+  return 0;
 }
